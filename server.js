8d88fd777d58cd3fee88303e9f9c605510d8f469
Hieu <ngxuanhieu97@gmail.com>
2023-01-08 10:10:51 +0700

    hello world

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const http = require('http');
+// required library
+const express = require('express')
+// create instance
+const app = express()
+// create port params
+const port = 3000
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>from Hieu Nguyen </h2>');
-    res.end();
+app.get('/', (req, res) => {
+  res.send('Hello World! 2023')
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.get('/home', (req, res) => {
+    res.send(`Welcome Home!`)
+  })
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
