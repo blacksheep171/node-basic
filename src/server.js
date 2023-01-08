921949658bd746458b696fad86bf5a35d7988145
Hieu <ngxuanhieu97@gmail.com>
2023-01-08 18:05:58 +0700

    static files

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 // required library
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+require('dotenv').config();
 // create instance
 const app = express()
-// create port params
-const port = 3000
+const port = process.env.PORT
 
+// console.log(">> check port: ", port);
+
+// console.log(process.env); 
 configViewEngine(app);
 app.get('/', (req, res) => {
   res.render('test/index.ejs')
@@ -15,7 +18,6 @@ app.get('/home', (req, res) => {
     res.send(`Welcome Home!`)
 })
   
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
