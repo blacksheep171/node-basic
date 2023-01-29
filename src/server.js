7248bce66f4aac3796f5d820af69129f0975b303
Hieu <ngxuanhieu97@gmail.com>
2023-01-30 00:46:44 +0700

    middleware

--- src/server.js
+++ src/server.js
@@ -4,13 +4,20 @@ import configViewEngine from './configs/viewEngine';
 import initWebRoute from "./route/web";
 import initAPIRoute from "./route/api";
 import connection from './configs/connectDB';
-
+var morgan = require('morgan');
 require('dotenv').config();
 // create instance
 const app = express()
 const port = process.env.PORT
 
+
+app.use((req, res, next) => {
+  console.log('>>> my middleware');
+  console.log(req.method);
+  next();
+});
 // declare middleware 
+app.use(morgan('combined'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -21,7 +28,12 @@ initWebRoute(app);
 
 //init api route
 initAPIRoute(app);
-  
+
+
+app.use((req, res) => {
+  return res.render('404.ejs');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
